5e7e23a7fa59c7daf596cdbea2a8f9efa5711dd3
Maria Jenkins <macjenkin@gmail.com>
2024-02-18 16:25:57 -0600

    add production build

---
+++ server/migrations/versions/5ad510fe6bfc_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5ad510fe6bfc
+Revises: c93a14d1aa19
+Create Date: 2024-02-18 15:52:35.436416
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5ad510fe6bfc'
+down_revision = 'c93a14d1aa19'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
